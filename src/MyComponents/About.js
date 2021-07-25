0d3bbd90f668d1f77e34230862e825fd7e459674
urvish <65525344+Patel-Urvish@users.noreply.github.com>
2021-07-25 12:28:05 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This is Todo-List app. Useful to schedule daily routine task.</p>
+        </div>
+    )
+}
